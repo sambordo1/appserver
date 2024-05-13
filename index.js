9914dbf09fdb2b0ddec595b8ddf31ad9dfe45788
Sam Bordo <sambordo1@gmail.com>
2024-05-13 17:15:11 -0400

    adding color option

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 import express from 'express';
+
 const app = express();
 
-app.listen(80, () => {
-    console.log("Hello from EC2 app-server on port 4000");
+app.listen(4000, () => {
+    console.log("Server listening on port 4000");
 });
 
 app.get('/', (req, res) => {
     res.json('Hello from EC2 app-server');
-})
+});
+
+app.get('/api/app-color', (req, res) => {
+    const color = req.query.color;
+
+    if (color === 'BLUE') {
+        res.send('Returning blue');
+    } else if (color === 'GREEN') {
+        res.send('Returning green');
+    } else {
+        res.status(400).send('Invalid color');
+    }
+});
